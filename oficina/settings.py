0ba6aef57f5ef67ab8befb7b1e2bc50d1a020dec
Alonso-Clemente <44202167+Alonso-Clemente@users.noreply.github.com>
2020-02-12 22:02:41 -0600

    NuevoRequirements eePleasi enter the commit message for your changes. Lines starting

--- oficina/settings.py
+++ oficina/settings.py
@@ -79,20 +79,11 @@ WSGI_APPLICATION = 'oficina.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'oficina',
-        'USER': 'solucion',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': ''
-
-        }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ 
 }
 
 
